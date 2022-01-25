4ef79bcb801bf6e3527a62104e1a132a15dca105
yoshangi <yoshanadesilva4@gmail.com>
2022-01-25 17:00:33 +0530

    add_user_id_column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_24_163943) do
+ActiveRecord::Schema.define(version: 2022_01_25_112903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 2022_01_24_163943) do
     t.string "type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_payments_on_user_id"
   end
 
   create_table "user_details", force: :cascade do |t|
@@ -97,4 +99,5 @@ ActiveRecord::Schema.define(version: 2022_01_24_163943) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "payments", "users"
 end
