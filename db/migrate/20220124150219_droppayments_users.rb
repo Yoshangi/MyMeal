d266dc8685096c1df33efd9dfeb789da0a760aea
yoshangi <yoshanadesilva4@gmail.com>
2022-02-15 00:57:01 +0530

    my account and singout

--- db/migrate/20220124150219_droppayments_users.rb
+++
@@ -1,5 +0,0 @@
-class DroppaymentsUsers < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :payments_users
-  end
-end
--- db/migrate/20220125132525_dropuser_details_users.rb
+++
@@ -1,5 +0,0 @@
-class DropuserDetailsUsers < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :user_details_users
-  end
-end
